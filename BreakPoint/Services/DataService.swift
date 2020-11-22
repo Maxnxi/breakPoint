7a130e694508922aca38d8602b5db394ce397e58
Mks <maxnxi@icloud.com>
2020-11-22 09:16:20 +0300

    gitIgnore added. Firen=base install

---
+++ BreakPoint/Services/DataService.swift
@@ -0,0 +1,16 @@
+//
+//  DataService.swift
+//  BreakPoint
+//
+//  Created by Maksim Ponomarev on 22.11.2020.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    static let instance = DataService()
+    
+}
